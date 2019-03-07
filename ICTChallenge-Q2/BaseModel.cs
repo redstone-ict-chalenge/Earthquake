ef2bfd321441fbf7d881bfd7944a4c67f74f0d1e
NGame <ngame1390@hotmail.com>
2019-03-07 10:18:07 +0330

    Initialize Commit

---
+++ ICTChallenge-Q2/BaseModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICTChallenge_Q2
+{
+    public class BaseModel : INotifyPropertyChanged
+    {
+        private string _title;
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string PropertyName) { PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName)); }
+        public string Title { get { return _title; } set { _title = value; OnPropertyChanged("Title"); } }
+    }
+}
